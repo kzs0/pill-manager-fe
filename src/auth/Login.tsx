e5def6d4fcefed0ceb2c33db39fa2837c2ed70e3
Kenzo Spaulding <kenzo.spaulding@gmail.com>
2025-02-23 20:27:20 -0800

    chore: Login button

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -3,7 +3,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button
+      onClick={() => loginWithRedirect()}
+      className="bg-gray-400 shadow-md align-middle hover:bg-gray-500 text-white font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700"
+    >
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
